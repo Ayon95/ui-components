2deeb6338fdc5a0397940d5f1e018a5402dc1ecd
Ayon95 <mushfiqurrahman78@yahoo.com>
2022-01-20 15:16:40 -0500

    added two more buttons in the modal, and implemented modal focus trap

--- modal/index.js
+++ modal/index.js
@@ -3,6 +3,36 @@ const buttonOpenModal = document.querySelector(".btn");
 const buttonCloseModal = document.querySelector(".btn-close");
 const modal = document.querySelector(".modal");
 
+const focusableElementsSelector =
+  'button, [href], input, textarea, select, [tabindex]:not([tabindex="-1"])';
+
+const modalFocusableElements = modal.querySelectorAll(
+  focusableElementsSelector
+);
+
+const modalFirstFocusableElement = modalFocusableElements[0];
+const modalLastFocusableElement =
+  modalFocusableElements[modalFocusableElements.length - 1];
+
+function manageModalFocus(e) {
+  if (e.key !== "Tab") return;
+
+  if (e.shiftKey) {
+    // if Shift + Tab is pressed, and the first element is focused, then move focus to the last focusable element
+    if (document.activeElement === modalFirstFocusableElement) {
+      modalLastFocusableElement.focus();
+      e.preventDefault();
+    }
+  } else {
+    // if the Tab key is pressed, and the last element is focused, move focus back to the first focusable element
+    if (document.activeElement === modalLastFocusableElement) {
+      modalFirstFocusableElement.focus();
+      // prevent the default browser action of moving to the next focusable element when Tab is pressed
+      e.preventDefault();
+    }
+  }
+}
+
 function openModal() {
   modal.classList.remove("hidden");
   // focus should be moved to the default focusable control in the modal
@@ -20,10 +50,13 @@ buttonOpenModal.addEventListener("click", openModal);
 modal.addEventListener("click", function (e) {
   const clickedInValidArea =
     e.target.classList.contains("modal") ||
-    e.target.classList.contains("btn-close");
+    e.target.classList.contains("btn-close") ||
+    e.target.classList.contains("btn");
   if (clickedInValidArea) closeModal();
 });
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") closeModal();
+  const modalIsOpen = !modal.classList.contains("hidden");
+  if (e.key === "Escape" && modalIsOpen) closeModal();
+  if (modalIsOpen) manageModalFocus(e);
 });
