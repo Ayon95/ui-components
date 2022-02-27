96d66cfbae3e137c52c80df55c036c84bae6b072
Ayon95 <mushfiqurrahman78@yahoo.com>
2022-02-27 01:07:33 -0500

    added JavaScript code to toggle dark mode and persist color preference in local storage

--- toggleSwitch/usingButton/index.js
+++ toggleSwitch/usingButton/index.js
@@ -1,6 +1,40 @@
 const toggleButton = document.querySelector(".toggle__button");
+const colorPreferenceLocalStorageKey =
+  "darkModeToggle_123_userSiteColorPreference";
 
-toggleButton.addEventListener("click", function (e) {
-  const isPressed = eval(e.currentTarget.getAttribute("aria-pressed"));
-  e.currentTarget.setAttribute("aria-pressed", !isPressed);
+function toggleDarkMode() {
+  const isPressed = eval(toggleButton.getAttribute("aria-pressed"));
+  toggleButton.setAttribute("aria-pressed", !isPressed);
+  document.body.classList.toggle("dark");
+}
+
+function saveColorPreferenceToLocalStorage() {
+  const isPressed = eval(toggleButton.getAttribute("aria-pressed"));
+  localStorage.setItem(
+    colorPreferenceLocalStorageKey,
+    isPressed ? "dark" : "light"
+  );
+}
+
+function setInitialColorMode() {
+  const userSiteColorPreference = localStorage.getItem(
+    colorPreferenceLocalStorageKey
+  );
+  const systemPreferenceIsDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  if (
+    userSiteColorPreference === "dark" ||
+    (!userSiteColorPreference && systemPreferenceIsDark)
+  ) {
+    toggleDarkMode();
+  }
+}
+
+setInitialColorMode();
+
+toggleButton.addEventListener("click", function () {
+  toggleDarkMode();
+  saveColorPreferenceToLocalStorage();
 });
